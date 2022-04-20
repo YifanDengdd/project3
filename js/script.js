31b0f9a2e5c48bfc76b42e9dafacafdf26cc4ef1
YifanDengdd <ydeng29@hawk.iit.edu>
2022-04-20 18:10:57 -0500

    2nd

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-function initMap(){
-    const IIT = {lat:41.8331335, lng: -87.6293096};
-    const googlemaps = new google.maps.Map(document.getElementById("map"),{
-        zoom: 6,
-        center: IIT,
-        mapTypeId: "terrain",
+function initMap() {
+    const castle = { lat: 54.6867, lng: 25.2906 }; 
+    const flag = { lat: 54.68, lng: 25.06 };
+    
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: castle,
     });
-    const marker = new google.maps.Marker({
-        position: IIT,
-        map:googlemaps,
+    const marker1 = new google.maps.Marker({
+      position: castle,
+      map: map,
     });
-    iitPath.setMap(googlemaps);
-}
+    
+    const marker2 = new google.maps.Marker({
+      position: flag,
+      map: map,
+      animation: google.maps.Animation.BOUNCE,
+      icon: 'images/lithuaniatiny.jpg'
+    });
+  }
+  
+  google.maps.event.addDomListener(window, 'load', initMap);
