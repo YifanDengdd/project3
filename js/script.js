cc6d2fc59e1c3f434f26d88dff663f2e56c67c9d
YifanDengdd <ydeng29@hawk.iit.edu>
2022-04-21 18:02:07 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 
-function initMap(){alert("ok");}
+function initMap() {
+    const kunming = { lat:24.877508,lng: 102.826872 }; 
+    const dianchi = { lat: 24.807832, lng: 102.699002 };
+    
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: kunming,
+    });
+    const marker1 = new google.maps.Marker({
+      position: kunming,
+      map: map,
+    });
+    
+    const marker2 = new google.maps.Marker({
+      position: dianchi,
+      map: map,
+      animation: google.maps.Animation.BOUNCE,
+      icon: 'image/dianChi.jpg'
+    });
+  }
+  
+  google.maps.event.addDomListener(window, 'load', initMap);
